250c8cfa505c3eaf0c881ead05be51cda755d871
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2020-05-10 14:03:16 -0400

    Add +/- delta from previous RA value

--- examples/drift_test.py
+++ examples/drift_test.py
@@ -13,7 +13,7 @@ def drift_test(iterations = 60, interval = 10):
   ra_min = 0.0
   count = 0
 
-  print('Time            OnStep Time   Sidereal      St  RA            DE        Equ                  RA"/min RA"Max DE"/min DE"Max')
+  print('Time            OnStep Time   Sidereal      St  RA            DE        Equ                  RAdlta RA"/min RA"Max DE"/min DE"Max')
 
   while True:
     # Increment the iteration counter
@@ -28,8 +28,8 @@ def drift_test(iterations = 60, interval = 10):
     equ_decimal  = config.scope.get_debug_equ()
 
     local_tm    = config.scope.get_time(True)
-    sidereal_tm = config.scope.get_sidereal_time(True)
     dt          = datetime.now().strftime('%H:%M:%S.%f')
+    sidereal_tm = config.scope.get_sidereal_time(True)
 
     status = 'N/A'
     if config.scope.is_slewing is True:
@@ -55,9 +55,24 @@ def drift_test(iterations = 60, interval = 10):
       de_drift = 0.0
       ra_drift_per_min = 0.0
       de_drift_per_min = 0.0
+      ra_prev = 0.0
+      de_prev = 0.0
+      ra_deviation = 0.0
+      de_deviation = 0.0
 
     else:
       # Record maximums and minimums
+      if ra_prev == 0.0:
+        ra_prev = ra
+      if de_prev == 0.0:
+        de_prev = de
+
+      ra_deviation = (ra - ra_prev) / 0.000278
+      de_deviation = (de - de_prev) / 0.000278
+
+      ra_prev = ra
+      de_prev = de
+
       if ra < ra_min:
         ra_min = ra
       if ra > ra_max:
@@ -87,7 +102,10 @@ def drift_test(iterations = 60, interval = 10):
     ra_max_drift = '{:6.2f}'.format(ra_drift)
     de_max_drift = '{:6.2f}'.format(de_drift)
 
-    print('%s %s %s %s %s %s %s %s %s %s %s' % (dt, local_tm, sidereal_tm, status, curr_ra, curr_de, equ_decimal, ra_drift_per_min, ra_max_drift, de_drift_per_min, de_max_drift))
+    ra_dev = '{:6.2f}'.format(ra_deviation)
+    de_dev = '{:6.2f}'.format(de_deviation)
+
+    print('%s %s %s %s %s %s %s %s %s %s %s %s' % (dt, local_tm, sidereal_tm, status, curr_ra, curr_de, equ_decimal, ra_dev, ra_drift_per_min, ra_max_drift, de_drift_per_min, de_max_drift))
     
     try:
       time.sleep(interval)
