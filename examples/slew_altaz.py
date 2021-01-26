b72cb835adf61e0c30714433dea08176072d0e37
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2021-01-26 14:03:36 -0500

    Check return status from setting alt/az coordinates

--- examples/slew_altaz.py
+++ examples/slew_altaz.py
@@ -15,7 +15,7 @@ def slew_altaz():
     print('Error turning tracking on: ', rc)
 
   rc = config.scope.set_target_alt(config.alt)
-  if rc == '0':
+  if rc == '1':
     print('Error setting target Alt: ', rc)
 
   rc = config.scope.set_target_azm(config.azm)
