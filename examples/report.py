11dc22d77580dde05cd3849ecf0ea9ed44f22ce8
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2021-12-05 13:08:31 -0500

    Display the mount's location, time, LST, ...etc. Fix initial mount status showing ---.

--- examples/report.py
+++ examples/report.py
@@ -9,9 +9,27 @@ import config
 
 def report(interval = 10):
 
-  while True:
-    status = '---'
+  # Display date, time, UTC offset, and latitude/longitude
+  dt  = config.scope.get_date()
+  tm  = config.scope.get_time()
+  ut  = config.scope.get_utc()
+  lt  = config.scope.get_latitude()
+  lg  = config.scope.get_longitude()
+  lst = config.scope.get_sidereal_time()
+  print('Date: %s Time: %s UTC %s Lat: %s Long: %s LST: %s' % (dt, tm, ut, lt, lg, lst))
 
+  print('Time     Stat Mnt-Time RA       DEC       Alt       Azm      ')
+  while True:
+
+    scope_tm = config.scope.get_time()
+    dt = datetime.now().strftime('%H:%M:%S')
+
+    curr_ra = config.scope.get_ra()
+    curr_de = config.scope.get_de()
+    curr_alt = config.scope.get_alt()
+    curr_azm = config.scope.get_azm()
+
+    status = '---'
     if config.scope.is_slewing is True:
       status = 'SLW'
       
@@ -21,14 +39,7 @@ def report(interval = 10):
     if config.scope.is_home is True:
       status = 'HOM'
 
-    scope_tm = config.scope.get_time()
-    dt = datetime.now().strftime('%H:%M:%S')
-    curr_ra = config.scope.get_ra()
-    curr_de = config.scope.get_de()
-    curr_alt = config.scope.get_alt()
-    curr_azm = config.scope.get_azm()
-
-    print('%s %s %s %s %s %s %s' % (dt, status, scope_tm, curr_ra, curr_de, curr_alt, curr_azm))
+    print('%s %s  %s %s %s %s %s' % (dt, status, scope_tm, curr_ra, curr_de, curr_alt, curr_azm))
     
     try:
       time.sleep(interval)
