f38d6c9754bb58f24265b17d367eda119206f6e7
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2022-01-01 17:08:48 -0500

    Display version of OnStep

--- examples/config.py
+++ examples/config.py
@@ -61,3 +61,5 @@ sys.path.append(os.getcwd())
 import lx200.onstep as onstep
 # Create a scope object
 scope = onstep.onstep(host = HOST, port = PORT)
+
+print('OnStep Version: ' + str(scope.get_version()))
