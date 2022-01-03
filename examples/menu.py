e4f7f917e8026d66d09b384306d6028902da8496
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2022-01-02 19:03:42 -0500

    Use letters for selecting from the menu. Numbers change often.

--- examples/menu.py
+++ examples/menu.py
@@ -21,34 +21,29 @@ import stress_test
 import drift_test
 
 menu = [
-    (init.init,                'Initialize - sets Coordinates, UTC offset, Date/Time'),
-    (home.home,                'Return to Home position'),
-    (report.report,            'Display mount status'),
-    (align.align,              '1 Star Align'),
-    (slew.slew,                'Slew to a star'),
-    (slew_altaz.slew_altaz,    'Slew to Alt/Azm'),
-    (drift_test.drift_test,    'Drift Test  - Test internal drift'),
-    (batch_align.batch_align,  'Batch Align - upload 9 alignment points'),
-    (stress_test.stress_test,  'Stress test - repeat long slews. Useful for testing driver/motor heating.'),
-    (polar_align.polar_align,  'Refine Polar Alignment'),
-    (backlash.backlash,        'Tune Backlash'),
-    (config.scope.dump_status, 'Dump Status'),
-    (sys.exit,                 'Exit'),
+    ('i',  init.init,                'Initialize - sets Coordinates, UTC offset, Date/Time'),
+    ('h',  home.home,                'Return to Home position'),
+    ('r',  report.report,            'Report mount status'),
+    ('a',  align.align,              '1 Star Align'),
+    ('s',  slew.slew,                'Slew to a star'),
+    ('sz', slew_altaz.slew_altaz,    'Slew to Alt/Azm'),
+    ('dt', drift_test.drift_test,    'Drift Test  - Test internal drift'),
+    ('ba', batch_align.batch_align,  'Batch Align - upload 9 alignment points'),
+    ('st', stress_test.stress_test,  'Stress test - repeat long slews. Useful for testing driver/motor heating.'),
+    ('pa', polar_align.polar_align,  'Refine Polar Alignment'),
+    ('bl', backlash.backlash,        'Tune Backlash'),
+    ('d',  config.scope.dump_status, 'Dump Status'),
+    ('x',  sys.exit,                 'Exit'),
   ]
 
 while True:
 
-  num = 0
   for row in menu:
-    num  = num + 1
-    text = row[1]
-
-    print(str(num) + '. ' + text)
+    print('%s.\t%s' % (row[0], row[2]))
 
   ans = input('Enter your selection -> ')
 
-  if ans.isnumeric():
-    sel = int(ans)
-    if sel > 0 and sel <= num :
-      func = menu[sel-1][0]
+  for row in menu:
+    if ans == row[0]:
+      func = row[1]
       func()
